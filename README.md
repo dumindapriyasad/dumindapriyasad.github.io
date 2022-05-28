691df8a235800156953ea3b355d9ec701c0165db
dumindapriyasad <duminda.priyasad@gmail.com>
2022-05-28 21:52:29 +0530

    Update Portfolio Website

---
+++ README.md
@@ -0,0 +1,5 @@
+## This is my portfolio website
+
+### Link
+
+[***dumindapriyasad.github.io***](https://dumindapriyasad.github.io/)
---
+++ js/script.js
@@ -0,0 +1,140 @@
+/*
+# My Portfolio
+*/
+
+/* Initialize Particles JS */
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 100,
+            density: {
+                enable: !0,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#ffffff"
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#000000"
+            },
+            polygon: {
+                nb_sides: 5
+            },
+            image: {
+                src: "img/github.svg",
+                width: 100,
+                height: 100
+            }
+        },
+        opacity: {
+            value: .5,
+            random: !1,
+            anim: {
+                enable: !1,
+                speed: 1,
+                opacity_min: .1,
+                sync: !1
+            }
+        },
+        size: {
+            value: 3,
+            random: !0,
+            anim: {
+                enable: !1,
+                speed: 40,
+                size_min: .1,
+                sync: !1
+            }
+        },
+        line_linked: {
+            enable: !0,
+            distance: 150,
+            color: "#ffffff",
+            opacity: .4,
+            width: 1
+        },
+        move: {
+            enable: !0,
+            speed: 6,
+            direction: "none",
+            random: !1,
+            straight: !1,
+            out_mode: "out",
+            bounce: !1,
+            attract: {
+                enable: !1,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: !0,
+                mode: "grab"
+            },
+            onclick: {
+                enable: !0,
+                mode: "push"
+            },
+            resize: !0
+        },
+        modes: {
+            grab: {
+                distance: 140,
+                line_linked: {
+                    opacity: 1
+                }
+            },
+            bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 8,
+                speed: 3
+            },
+            repulse: {
+                distance: 200,
+                duration: .4
+            },
+            push: {
+                particles_nb: 4
+            },
+            remove: {
+                particles_nb: 2
+            }
+        }
+    },
+    retina_detect: !0
+});
+
+/* Initialize Anime JS */
+anime({
+    targets: '.avatar',
+    rotate: '1turn',
+    easing: 'linear',
+    duration: 800
+});
+
+/* Initialize Typed JS */
+let typed = new Typed('#typed', {
+    strings: [
+        "I'm a ❝Developer❞",
+        "I like making fun, interactive things with <code>❝code❞</code>",
+        "Software Engineering Undergraduate",
+        "Tech Enthusiast | Optimist | Passionate Learner"
+    ],
+    typeSpeed: 70,
+    backSpeed: 35,
+    backDelay: 500,
+    startDelay: 1200,
+    loop: true,
+    showCursor: true,
+    cursorChar: '_'
+});
